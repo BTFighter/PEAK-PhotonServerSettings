ed50cd8aed00d90583d255854ac19da945381180
Kirigiri's <kiriha301@gmail.com>
2025-08-07 21:49:54 +0200

    Improved the code

--- NekogiriPeakOffline/NekogiriPeakOffline.cs
+++ NekogiriPeakOffline/NekogiriPeakOffline.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.IO;
 using BepInEx;
 using HarmonyLib;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 using Zorro.Core;
+using UnityEngine.Networking;
 
 namespace NekogiriMod
 {
@@ -12,7 +15,6 @@ namespace NekogiriMod
     {
         private void Awake()
         {
-            // Set up plugin logging
             Logger.LogInfo(@"
  ██ ▄█▀ ██▓ ██▀███   ██▓  ▄████  ██▓ ██▀███   ██▓
  ██▄█▒ ▓██▒▓██ ▒ ██▒▓██▒ ██▒ ▀█▒▓██▒▓██ ▒ ██▒▓██▒
@@ -30,14 +32,13 @@ namespace NekogiriMod
             var harmony = new Harmony("kirigiri.peak.nekogirioffline");
             harmony.PatchAll();
 
-            // Optionally log that the patch has been applied
             Logger.LogInfo("Made with <3 By Kirigiri \nhttps://discord.gg/TBs8Te5nwn");
         }
 
         [HarmonyPatch(typeof(CloudAPI), nameof(CloudAPI.CheckVersion))]
         public class CloudAPICheckVersionPatch
         {
-            private static int? _cachedLevelIndex = null;
+            private static readonly DateTime startDate = new DateTime(2025, 6, 14);
 
             [HarmonyPrefix]
             public static bool Prefix(Action<LoginResponse> response)
@@ -46,15 +47,12 @@ namespace NekogiriMod
 
                 BuildVersion buildVersion = new BuildVersion(Application.version);
 
-                if (_cachedLevelIndex == null)
-                {
-                    System.Random rng = new System.Random();
-                    _cachedLevelIndex = rng.Next(0, int.MaxValue);
-                }
+                DateTime today = DateTime.UtcNow.Date; // Use DateTime.Now.Date if local time is preferred
+                int daysSinceStart = (today - startDate).Days;
+                int levelIndex = Mathf.Max(1, daysSinceStart + 1);
 
                 LoginResponse loginResponse;
 
-                // Assuming you have access to buildVersion object here
                 if (buildVersion.BuildName == "beta")
                 {
                     loginResponse = new LoginResponse
@@ -63,7 +61,7 @@ namespace NekogiriMod
                         HoursUntilLevel = 24,
                         MinutesUntilLevel = 0,
                         SecondsUntilLevel = 0,
-                        LevelIndex = _cachedLevelIndex.Value,
+                        LevelIndex = levelIndex,
                         Message = "Thanks for testing the PEAK beta. Watch out for bugs! (the current beta is the same as the live game, check back later for a new beta!)"
                     };
                 }
@@ -75,7 +73,7 @@ namespace NekogiriMod
                         HoursUntilLevel = 24,
                         MinutesUntilLevel = 0,
                         SecondsUntilLevel = 0,
-                        LevelIndex = _cachedLevelIndex.Value,
+                        LevelIndex = levelIndex,
                         Message = "why did the chicken cross the caldera?"
                     };
                 }
@@ -91,8 +89,51 @@ namespace NekogiriMod
             [HarmonyPrefix]
             public static bool Prefix()
             {
+                Debug.Log("[NekogiriPeak] Patching NetworkConnector.ConnectToPhoton");
 
                 PhotonNetwork.OfflineMode = true;
+                BuildVersion version = new BuildVersion(Application.version);
+                PhotonNetwork.AutomaticallySyncScene = true;
+                PhotonNetwork.GameVersion = version.ToString();
+                PhotonNetwork.PhotonServerSettings.AppSettings.AppVersion = version.ToMatchmaking();
+
+                var method = typeof(NetworkConnector).GetMethod("PrepareSteamAuthTicket", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+                if (method != null)
+                {
+                    method.Invoke(null, new object[] {
+                new Action(() =>
+                {
+                    PhotonNetwork.ConnectUsingSettings();
+                    Debug.Log("Photon Start " + PhotonNetwork.NetworkClientState.ToString() +
+                              " using app version: " + version.ToMatchmaking());
+                })
+            });
+                }
+                else
+                {
+                    Debug.LogError("[NekogiriPeak] Failed to find PrepareSteamAuthTicket via reflection");
+                }
+
+                return false;
+            }
+        }
+        [HarmonyPatch(typeof(PhotonNetwork), nameof(PhotonNetwork.ConnectUsingSettings), new[] { typeof(AppSettings), typeof(bool) })]
+        public class PhotonNetworkPatch
+        {
+            [HarmonyPrefix]
+            public static bool Prefix(AppSettings appSettings, bool startInOfflineMode, ref bool __result)
+            {
+                Debug.Log("[NekogiriPeak] Overriding PhotonNetwork.ConnectUsingSettings");
+
+                if (startInOfflineMode || PhotonNetwork.OfflineMode)
+                {
+                    PhotonNetwork.OfflineMode = true;
+                    Debug.LogWarning("Kirigiri disabled the online mode, going offline !");
+                    __result = true;
+                    return false;
+                }
+
+                __result = false;
                 return false;
             }
         }
