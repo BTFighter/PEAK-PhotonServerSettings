a464aa79785191506ee62ebfb2390630fc85dd5c
Kirigiri's <kiriha301@gmail.com>
2025-08-07 13:28:00 +0200

    Add project files.

---
+++ NekogiriPeakOffline/NekogiriPeakOffline.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using Photon.Pun;
+using UnityEngine;
+using Zorro.Core;
+
+namespace NekogiriMod
+{
+    [BepInPlugin("kirigiri.peak.nekogirioffline", "NekogiriPeakOffline", "1.0.0.0")]
+    public class NekogiriMod : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Set up plugin logging
+            Logger.LogInfo(@"
+ ██ ▄█▀ ██▓ ██▀███   ██▓  ▄████  ██▓ ██▀███   ██▓
+ ██▄█▒ ▓██▒▓██ ▒ ██▒▓██▒ ██▒ ▀█▒▓██▒▓██ ▒ ██▒▓██▒
+▓███▄░ ▒██▒▓██ ░▄█ ▒▒██▒▒██░▄▄▄░▒██▒▓██ ░▄█ ▒▒██▒
+▓██ █▄ ░██░▒██▀▀█▄  ░██░░▓█  ██▓░██░▒██▀▀█▄  ░██░
+▒██▒ █▄░██░░██▓ ▒██▒░██░░▒▓███▀▒░██░░██▓ ▒██▒░██░
+▒ ▒▒ ▓▒░▓  ░ ▒▓ ░▒▓░░▓   ░▒   ▒ ░▓  ░ ▒▓ ░▒▓░░▓  
+░ ░▒ ▒░ ▒ ░  ░▒ ░ ▒░ ▒ ░  ░   ░  ▒ ░  ░▒ ░ ▒░ ▒ ░
+░ ░░ ░  ▒ ░  ░░   ░  ▒ ░░ ░   ░  ▒ ░  ░░   ░  ▒ ░
+░  ░    ░     ░      ░        ░  ░     ░      ░  
+                                                 
+");
+            Logger.LogInfo("NekogiriPeakOffline has loaded!");
+
+            var harmony = new Harmony("kirigiri.peak.nekogirioffline");
+            harmony.PatchAll();
+
+            // Optionally log that the patch has been applied
+            Logger.LogInfo("Made with <3 By Kirigiri \nhttps://discord.gg/TBs8Te5nwn");
+        }
+
+        [HarmonyPatch(typeof(CloudAPI), nameof(CloudAPI.CheckVersion))]
+        public class CloudAPICheckVersionPatch
+        {
+            private static int? _cachedLevelIndex = null;
+
+            [HarmonyPrefix]
+            public static bool Prefix(Action<LoginResponse> response)
+            {
+                Debug.Log("[NekogiriPeak] Patching CloudAPI.CheckVersion");
+
+                BuildVersion buildVersion = new BuildVersion(Application.version);
+
+                if (_cachedLevelIndex == null)
+                {
+                    System.Random rng = new System.Random();
+                    _cachedLevelIndex = rng.Next(0, int.MaxValue);
+                }
+
+                LoginResponse loginResponse;
+
+                // Assuming you have access to buildVersion object here
+                if (buildVersion.BuildName == "beta")
+                {
+                    loginResponse = new LoginResponse
+                    {
+                        VersionOkay = true,
+                        HoursUntilLevel = 24,
+                        MinutesUntilLevel = 0,
+                        SecondsUntilLevel = 0,
+                        LevelIndex = _cachedLevelIndex.Value,
+                        Message = "Thanks for testing the PEAK beta. Watch out for bugs! (the current beta is the same as the live game, check back later for a new beta!)"
+                    };
+                }
+                else
+                {
+                    loginResponse = new LoginResponse
+                    {
+                        VersionOkay = true,
+                        HoursUntilLevel = 24,
+                        MinutesUntilLevel = 0,
+                        SecondsUntilLevel = 0,
+                        LevelIndex = _cachedLevelIndex.Value,
+                        Message = "why did the chicken cross the caldera?"
+                    };
+                }
+
+                response?.Invoke(loginResponse);
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(NetworkConnector), nameof(NetworkConnector.ConnectToPhoton))]
+        public class NetworkConnectorPatch
+        {
+            [HarmonyPrefix]
+            public static bool Prefix()
+            {
+
+                PhotonNetwork.OfflineMode = true;
+                return false;
+            }
+        }
+    }
+}
